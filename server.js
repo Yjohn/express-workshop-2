457510a28c0a025aadc9017382ef0ca0462aea7d
YJohn <yfekadu2016@gmail.com>
2017-07-29 13:56:59 +0100

    create handlebars for all the html files

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
+app.use(express.static("public", { 'extensions': ['html'] }));
 
+app.get("/", function (req, res) {
+  res.render("index", {
+    title: 'Yohannes Profile',
+  });
+});
 
+app.get("/my-cv", function (req, res) {
+  res.render("my-cv");
+});
+
+app.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
